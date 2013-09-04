cb7be312089e95209e07492c537abb43d67f84a5
Bruna Xavier <brunabxsantos@gmail.com>
2013-09-03 23:37:01 -0300

    Script para busca de imagem

---
+++ main.py
@@ -0,0 +1,15 @@
+from descriptionAnalyser import DescriptionAnalyser
+from imageSearch import ImageSearch
+
+# captura a descricao
+description = raw_input('Descricao:\n')
+
+# extrai palavras-chave
+analysis = DescriptionAnalyser(description)
+analysis.calculate_terms_frequency()
+
+# download de imagens
+for term in analysis.terms():
+    ImageSearch.search(term)
+
+
